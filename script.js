f2862d4635e56a9b039dd4b006ca2b919ccc79b7
Robijsss <robijs.lejins@marupe.edu.lv>
2025-04-30 11:44:45 +0000

    fsafjasfaskn

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function login(){
   const user = document.getElementById('user').value;
   const epasts = document.getElementById('epasts').value;
   const pass = document.getElementById('pass').value;
-  if(user=="Robijs" && epasts=="robijslejins@gmail.com" && pass=="Robijaparole232"){
+  if(user=="Robijs" && pass=="Robijaparole232"){
     alert('Jūs esat veiksmīgi ielogojies!');
   }else{
     alert('Nepareizs lietotājvārds vai parole!');
