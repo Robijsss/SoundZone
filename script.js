67f01bdf403a4b930026a63e143a1086a5b5cf64
Robijsss <robijs.lejins@marupe.edu.lv>
2025-04-30 11:25:01 +0000

    njkfgdfjghjdf

---
+++ script.js
@@ -0,0 +1,10 @@
+function login(){
+  const user = document.getElementById('user').value;
+  const epasts = document.getElementById('epasts').value;
+  const pass = document.getElementById('pass').value;
+  if(user=="Robijs" && epasts=="robijslejins@gmail.com" && pass=="Robijaparole232"){
+    alert('Jūs esat veiksmīgi ielogojies!');
+  }else{
+    alert('Nepareizs lietotājvārds vai parole!');
+  }
+}
