c2c44408a473df1a8dc9d86bbbfc6cb013dbf553
Robijsss <robijs.lejins@marupe.edu.lv>
2025-05-07 12:11:14 +0000

    sssss

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 function login(){
   const user = document.getElementById('user').value;
-  const epasts = document.getElementById('epasts').value;
   const pass = document.getElementById('pass').value;
   if(user=="Robijs" && pass=="Robijaparole232"){
     alert('Jūs esat veiksmīgi ielogojies!');
